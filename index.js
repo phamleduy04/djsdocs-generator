44713780b9f939755a6f686edb574729402e9ffb
Duy Pham Le <phamleduy04@gmail.com>
2020-11-10 15:28:19 -0600

    default should be stable

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const baseURL = 'https://djsdocs.sorta.moe/v2/embed';
 const acceptedBranches = ['rpc', 'master', 'commando', 'stable'];
 module.exports = {
     /**
-     * @param {String} branch to search (default master)
+     * @param {String} branch to search (default stable)
      * @param {String} query to Search
      * @returns {Promise}
      */
-    search: function(branch = 'master', query) {
+    search: function(branch = 'stable', query) {
         if (!query) throw new TypeError('Query not specified!');
         branch = branch.toLowerCase();
         if (!acceptedBranches.includes(branch)) throw new Error('branch should be rpc, master, commando or stable');
