0bd5f2f8d27f22e8fbace3af479668abffc0d1a9
Duy Pham Le <phamleduy04@gmail.com>
2021-01-14 10:26:07 -0600

    fix bugs

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ module.exports = {
         branch = branch.toLowerCase();
         if (!acceptedBranches.includes(branch)) throw new Error('branch should be rpc, master, commando or stable');
         return new Promise((resolve, reject) => {
-            fetch(`${baseURL}?src=${branch}&q=${query}`)
+            fetch(`${baseURL}?src=${branch}&q=${encodeURIComponent(query)}`)
                 .then(res => res.json())
                 .then(body => {
                     if (body === null) reject('Not found!');
